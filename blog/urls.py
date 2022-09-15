e475c3198d0855d66df018a3035774a850cbc2ed
ed-Dan <danil.tverdokhlib@nure.ua>
2022-09-15 16:22:28 +0300

    changed urls . now it works normal

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
     path('', views.home, name='home'),
-    path('/signup', views.SignUp.as_view(), name='signup'),
+    path('signup', views.SignUp.as_view(), name='signup'),
 ]
